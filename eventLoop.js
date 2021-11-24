d9b2e9173e37dfbb1d2806ae1391ec3e0f3f7dd8
mukul0000 <mukulrajput9675@gmail.com>
2021-11-24 14:29:56 +0530

    done

---
+++ eventLoop.js
@@ -0,0 +1,15 @@
+console.log("let start");
+
+const fun2 = () => {
+	setTimeout(() => {
+		console.log("fun2 is starting");
+	}, 3000);
+};
+
+const fun1 = () => {
+	console.log("fun1 is runing");
+	fun2();
+	console.log("fun1 is ending");
+};
+
+fun1();
